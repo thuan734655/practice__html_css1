349343be4a92a3e3d090b4710d8c376a04378085
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-06-09 10:40:41 +0700

    code animate #1

--- src/script/main.js
+++ src/script/main.js
@@ -20,3 +20,33 @@ header__button__left.addEventListener("mouseleave", function (event) {
   header__button__right.style.backgroundColor = "rgba(93, 80, 198, 1)";
   header__button__left.style.boxShadow = "none";
 });
+document.addEventListener("DOMContentLoaded", function() {
+  var animatedElements = [
+    ".section-services__content--left",
+    ".section-services__content--right",
+    ".section-destination__content--top",
+    ".section-destination__content--bottom",
+    ".top-content--right",
+    ".section-travelPoint__content--right",
+    ".section-travelPoint__content--left",
+    ".section-keyFeatures__container--left",
+    ".section-keyFeatures__container--right"
+  ];
+  animatedElements.forEach(function(elementClass) {
+    var animatedElement = document.querySelector(elementClass);
+    setAnimate(animatedElement);
+  });
+});
+
+
+var setAnimate = (animatedElement)=> {
+  window.addEventListener("scroll", function() {
+     // tra ve khoang cach tu top cua phan tu toi viewport
+    var elementTop = animatedElement.getBoundingClientRect().top;
+    var windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight * 0.85) {
+        animatedElement.classList.add("slide-up");
+    }
+});
+}
